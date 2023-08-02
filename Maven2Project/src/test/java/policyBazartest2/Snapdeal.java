c5873f9768d0516c099f56815d2803498291c9a0
pallavigawali <gawalipallavi20@gmail.com>
2023-08-02 14:59:03 +0530

    [pallavi]added new class--> Snapdeal

---
+++ Maven2Project/src/test/java/policyBazartest2/Snapdeal.java
@@ -0,0 +1,13 @@
+package policyBazartest2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Snapdeal {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		  driver.get("https://www.snapdeal.com/");
+	}
+
+}
