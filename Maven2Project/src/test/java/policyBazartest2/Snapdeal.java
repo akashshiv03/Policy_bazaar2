6e16135fbefdd0aa832b5e0362d1a709dcfe0460
akashshiv03 <akashshivram03@gmail.com>
2023-08-03 15:10:44 +0530

    [ak]jkk

--- Maven2Project/src/test/java/policyBazartest2/Snapdeal.java
+++ Maven2Project/src/test/java/policyBazartest2/Snapdeal.java
@@ -9,6 +9,8 @@ public class Snapdeal {
 		WebDriver driver=new ChromeDriver();
 		  driver.get("https://www.snapdeal.com/");
 		  System.out.println("hjgjikuyjgbikhh");
+		  System.out.println("xxxssdd,dmfffd");
+		  
 	}
 
 }
